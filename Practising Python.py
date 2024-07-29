00bbd758dded46d4891f2fcf0b0b8d18834490c8
EwetheQuant <140906962+EwetheQuant@users.noreply.github.com>
2024-07-28 21:46:01 -0400

    upload a py file
    Uploaded a py file from my laptop

---
+++ Practising Python.py
@@ -0,0 +1,46 @@
+print("God is faithful")
+2+2
+50 - 5*6
+(50-5)*6
+50 - 5*6 / 4
+(50 - 5*6) / 4
+8 / 5
+17 // 3
+17 / 3
+17 % 3
+5*3+2
+5**2 # ** means raised to the power 5 raised to the power of 2
+10 ** 10 # 10 to the power of 10
+width = 20
+height = 5 * 9
+width * height
+_ + 100
+# _ is used to call the last printed expression - this means that _ is a variable assigned to the last printed expression
+cost = 589
+0.13 * cost
+round(_ , 2)
+cost + _
+'doesn\'t'
+'parish'
+"doesn't"
+print("doesn't")
+print("some\ncat")
+print(r"some\ncat")
+# r is used to make \ not to mean anything special
+# \ is used to escape quotes
+print('''\
+Usage: thingy [OPTIONS]
+-h Display this usage message
+-H hostname Hostname to connect to
+''')
+print("""\
+Usage: thingy [OPTIONS]
+     -h                        Display this usage message
+     -H hostname               Hostname to connect to
+""")
+3*'un'+'ium'
+3 * 'un' + 'ium'
+3 * 'un' + 'ium'
+'Py' 'thon'
+"Python"
+"Py" "thon"
