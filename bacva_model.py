2403b90ee86bb92cc12bec16e74f1702f60f71ca
EwetheQuant <140906962+EwetheQuant@users.noreply.github.com>
2024-07-28 21:43:51 -0400

    Create bacva_model.py
    This is a Python Code implementation of the BA-CVA model for calculating CVA capital requirement.

---
+++ bacva_model.py
@@ -0,0 +1,10 @@
+# This is the BA-CVA model prescribed by OSFI for the calculation of CVA capital requirement
+# The following variables are required:
+  Netting Set EAD
+  Netting Set Effective Maturity
+  Counterparty Sector
+  Counterparty Credit Rating
+  OSFI prescribed Counterparty Risk Weight mapping table
+  Single Named CDS Hedges: Notional, Maturity, Underlying Name, Underlying Name Sector, Underlying Name Credit Rating
+  Index CDS Hedges: Notional, Maturity, Constituents, Constituents Sector, Constituents Credit Rating
+# Calculate the SCVAc - Counterparty Standalone CVA capital
