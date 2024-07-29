f19ec4ca212e04352b3e6f47551214ae4662f1f1
EwetheQuant <140906962+EwetheQuant@users.noreply.github.com>
2024-07-28 22:13:33 -0400

    Create saccr_ead_model.py
    Python code implementation of the SA-CCR EAD model

---
+++ saccr_ead_model.py
@@ -0,0 +1,9 @@
+# Banks are required by regulators to set aside capital for Counterparty Credit Risk
+# Basel has prescribed the SA-CCR EAD model for estimating the Exposure At Default for Counterparty Credit Risk
+# There are two main components of the model:
+
+  RC: Replacement Cost
+  PFE: Potential Future Exposure
+
+  EAD = 1.4 * (RC + PFE)
+  
